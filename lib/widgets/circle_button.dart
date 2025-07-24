7fd27c42343538780affcb26cc82e302c0b03341
Amal Anan <133678416+amalanan@users.noreply.github.com>
2025-07-24 21:40:39 +0300

    Create circle_button.dart

---
+++ lib/widgets/circle_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CircleButton extends StatelessWidget {
+  const CircleButton({super.key, required this.icon, required this.iconSize, required this.onPressed});
+
+  final IconData icon;
+  final double iconSize;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(6.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        shape: BoxShape.circle,
+      ),
+      child: IconButton(
+        onPressed: onPressed,
+        icon: Icon(icon),
+        iconSize: iconSize,
+        color: Colors.black,
+      ),
+    );
+  }
+}
